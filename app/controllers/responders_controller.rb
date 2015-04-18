c0a63bc97d58c7e61a2beb6663c42d277ac446fd
Henrik Nygren <nygrenh@gmail.com>
2015-04-18 15:43:07 +0300

    Imple Implement stuff to responders

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,7 +1,10 @@
 class RespondersController < ApplicationController
+  def index
+    render json: Responder.all
+  end
 
   def create
-    responder = Responder.new(responder_params)
+    responder = Responder.new(responder_params(:on_duty))
     if responder.save
       render json: responder, status: :created
     else
@@ -9,14 +12,27 @@ class RespondersController < ApplicationController
     end
   end
 
+  def show
+    responder = Responder.find_by(name: params[:name])
+    if responder
+      render json: responder
+    else
+      head :not_found
+    end
+  end
+
   def update
     responder = Responder.find_by(name: params[:name])
-    responder.update(responder_params)
+    if responder.update(responder_params(:name, :capacity, :type))
+      render json: responder
+    else
+      head :unprocessable_entity
+    end
   end
 
   private
 
-  def responder_params
-    params.require(:responder).permit(:type, :name, :capacity)
+  def responder_params(*without)
+    params.require(:responder).permit([:type, :name, :capacity, :on_duty] - without)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   post 'responders', to: 'responders#create'
   patch 'responders/:name', to: 'responders#update'
+  get 'responders', to: 'responders#index'
+  get 'responders/:name', to: 'responders#show'
+
   post 'emergencies', to: 'emergencies#create'
   get 'emergencies', to: 'emergencies#index'
   patch 'emergencies', to: 'emergencies#index'
