349c25977f37025e0e9153fc5a537f131ebd5b5e
Henrik Nygren <nygrenh@gmail.com>
2015-04-25 01:52:04 +0300

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,9 @@ Rails.application.routes.draw do
   get 'responders/new', to: 'errors#error_404'
   get 'emergencies/new', to: 'errors#error_404'
 
-  post 'responders', to: 'responders#create'
-  patch 'responders/:name', to: 'responders#update'
-  get 'responders', to: 'responders#index'
-  get 'responders/:name', to: 'responders#show'
+  resources :responders, except: [:destroy, :new, :edit], param: :name
 
-  post 'emergencies', to: 'emergencies#create'
-  get 'emergencies', to: 'emergencies#index'
-  patch 'emergencies', to: 'emergencies#index'
-  get 'emergencies/:code', to: 'emergencies#show'
-  patch 'emergencies/:code', to: 'emergencies#update'
+  resources :emergencies, except: [:destroy, :new, :edit], param: :code
 
   match '*a', to: 'errors#error_404', via: :all
 end
